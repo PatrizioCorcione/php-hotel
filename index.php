cb9e948b0ecab5704743cc623c34bd77e2e30128
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-04-15 13:57:02 +0200

    [add] logica stampo in pagina

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+$hotels = [
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
+];
+
+foreach ($hotels as $hotel) {
+  $nameP = $hotel['name'];
+  $descriptionP = $hotel['description'];
+  $parkingP = ($hotel['parking']) ? 'Si' : 'No';
+  $votep = $hotel['vote'];
+  $distanceP = $hotel['distance_to_center'];
+  echo "<h3>$nameP</h3>", "<h3>$descriptionP</h3>", "<h3>Parcheggio:$parkingP</h3>", "<h3>$voteP</h3>", "<h3>$distranceP</h3>";
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
